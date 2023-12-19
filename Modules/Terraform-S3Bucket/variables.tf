be380856c88d349364c0340e0c1e8dc329cc668c
Vikram Revanna <vikram.k@mastek.com>
2023-12-19 20:13:25 +0530

    modified code

--- Modules/Terraform-S3Bucket/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "is_enable" {}
-variable "s3bucket_count" {}
-variable "bucket" {}
-variable "force_destroy" {}
-variable "environment" {}
-
-variable "bucket_acl" {
-    default = "private"
-}

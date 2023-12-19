aa5741311fc89279a95fe57d085d8c794f25d42e
Vikram Revanna <vikram.k@mastek.com>
2023-12-19 20:03:17 +0530

    code modified

--- IAM-Roles/role.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_role" "iam-role" {
-  name               = var.role_name
-  assume_role_policy = file("${path.module}/${var.role_policy_file}")
-}
--- S3-Bucket/main.tf
+++
@@ -1,9 +0,0 @@
-module "s3-bucket" {
-  source = "../Modules/Terraform-S3Bucket"
-
-  is_enable      = var.is_bucket_enabled
-  s3bucket_count = 1
-  bucket         = var.bucket_name
-  force_destroy  = var.force_destroy
-  environment    = var.env
-}

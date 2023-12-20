15227ff34950a938fcd053532580db6efb1434c1
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-20 23:47:11 +0530

    Update backend.tf

--- ECS-fargate/backend.tf
+++ ECS-fargate/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-ews-baket1"
     region         = "us-east-1"
-    key            = "Modularized/ECS-fargate/terraform.tfstate"
+    key            = "Modularized/ECS-Fargate/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }

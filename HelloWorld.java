d29b07af04eaa4dc820c51f969094a251a828fe2
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-21 00:08:29 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+// This is a simple Java program. 
+// FileName : "HelloWorld.java". 
+  
+class HelloWorld { 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    {
+        String html = """
+              <html>
+                  <body>
+                      <p>Hello, world</p>
+                  </body>
+              </html>
+              """;
+        System.out.println(html);
+    } 
+}

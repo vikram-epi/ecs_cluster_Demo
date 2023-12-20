3421249110958da138a8d5a2529845547fb89b2d
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-20 23:55:47 +0530

    Update backend.tf

--- Modules/Terraform-ECS-Fargate/backend.tf
+++ Modules/Terraform-ECS-Fargate/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "my-ews-baket8780"
     region         = "us-east-1"
     key            = "Modularized/ECS-Fargate/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
     encrypt = true
   }
   required_version = ">=0.13.0"

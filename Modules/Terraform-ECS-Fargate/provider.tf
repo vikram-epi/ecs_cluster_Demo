592711d9706f5fd705140afc995acae2aefc6861
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-21 11:05:13 +0530

    Update provider.tf

--- Modules/Terraform-ECS-Fargate/provider.tf
+++ Modules/Terraform-ECS-Fargate/provider.tf
@@ -1,10 +1,4 @@
-terraform {
-  required_version = ">=0.13.0"
-  required_providers {
-    aws = {
-      version = ">= 2.7.0"
-      source  = "hashicorp/aws"
-      region = "us-east-1"
-    }
-  }
+provider "aws" {
+  region  = "us-east-1"
 }
+

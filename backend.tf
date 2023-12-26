00736670bf35ffb21d0eb64087ca862a033d5b52
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-26 16:43:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,3 +1,15 @@
-module "Terraform-ECS-Fargate" {
-  source = "./Modules/Terraform-ECS-Fargate" 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         	   = "my-ews-baket8780"
+    key              	   = "s3://my-ews-baket8780/Modularized/ECS-Fargate/terraform.tfstate"
+    region         	   = "us-east-1"
+    encrypt        	   = true
+  }
 }

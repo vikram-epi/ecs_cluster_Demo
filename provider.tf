1218061f9dfa366c38f3e6d3ac90ffa9409ecdc0
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-26 14:49:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,14 @@
 terraform {
-  backend "s3" {
-    bucket = "my-ews-baket8780"
-    key    = "s3://my-ews-baket8780/Modularized/ECS-Fargate/terraform.tfstate"
-    region = "us-east-1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
 }
 

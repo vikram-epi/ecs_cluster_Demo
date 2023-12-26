fecb5c4618fabfca4381a3b444cd369b6550648b
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-26 14:09:03 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,15 @@
 terraform {
-required_version = ">=0.13.0"
 backend "s3" {
     bucket         = "my-ews-baket8780"
     region         = "us-east-1"
     key            = "Modularized/ECS-Fargate/terraform.tfstate"
     encrypt        = true
+  }
+required_version = ">=0.13.0"
+required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
   }
 }

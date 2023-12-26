4ba15ef889970e416a0d9430f988327ab1c3d75f
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-26 15:01:47 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,6 @@
-terraform {
-  backend "s3" {
+backend "s3" {
     bucket = "my-ews-baket8780"
     key    = "s3://my-ews-baket8780/Modularized/ECS-Fargate/terraform.tfstate"
     region = "us-east-1"
   }
-}
 

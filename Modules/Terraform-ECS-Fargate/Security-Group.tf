3869c3c6ba4f0636eced039830f56777ba29f2c9
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-28 05:15:23 +0530

    Update Security-Group.tf

--- Modules/Terraform-ECS-Fargate/Security-Group.tf
+++ Modules/Terraform-ECS-Fargate/Security-Group.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "SG1" {
   name        = "Main-SG1"
   description = "Allow Port 80"
-  vpc_id      = aws_vpc.vpc.id
+  vpc_id      = aws_vpc.vpc1.id
 
   ingress {
     from_port   = 80

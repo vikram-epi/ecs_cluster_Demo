0397a7cb1d2726032bc65fc8ca66eb2834ee8d28
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-28 05:17:15 +0530

    Update Load-Balancer.tf

--- Modules/Terraform-ECS-Fargate/Load-Balancer.tf
+++ Modules/Terraform-ECS-Fargate/Load-Balancer.tf
@@ -1,8 +1,8 @@
-resource "aws_lb" "LB" {
+resource "aws_lb" "LB1" {
   name               = "Main-LB1"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.SG.id]
+  security_groups    = [aws_security_group.SG1.id]
   subnets            = [aws_subnet.subnet12.id, aws_subnet.subnet12.id]
 
   tags = {
@@ -11,12 +11,12 @@ resource "aws_lb" "LB" {
 }
 
 resource "aws_alb_listener" "Listener" {
-  load_balancer_arn = aws_lb.LB.id
+  load_balancer_arn = aws_lb.LB1.id
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = aws_lb_target_group.TG.id
+    target_group_arn = aws_lb_target_group.TG1.id
     type             = "forward"
   }
 }

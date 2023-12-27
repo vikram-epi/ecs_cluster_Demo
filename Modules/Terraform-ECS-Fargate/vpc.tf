89dbd0729550ce013586f1f07da61e52789f7d6b
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-28 05:21:46 +0530

    Update vpc.tf

--- Modules/Terraform-ECS-Fargate/vpc.tf
+++ Modules/Terraform-ECS-Fargate/vpc.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "subnet11" {
 
 
 resource "aws_subnet" "subnet12" {
-  vpc_id                  = aws_vpc1.vpc.id
+  vpc_id                  = aws_vpc.vpc1.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1b"

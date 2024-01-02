8b0c9ad1479882fe7a001ded3b290c0e79845d1c
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2024-01-02 07:48:36 +0530

    Update ecs-service.tf

--- Modules/Terraform-ECS-Fargate/ecs-service.tf
+++ Modules/Terraform-ECS-Fargate/ecs-service.tf
@@ -1,9 +1,9 @@
-resource "aws_ecs_service" "ECS-Service" {
-  name                               = "First-Service1"
+resource "aws_ecs_service" "ECS-Service1" {
+  name                               = "First-Service2"
   launch_type                        = "FARGATE"
   platform_version                   = "LATEST"
-  cluster                            = aws_ecs_cluster.ECS1.id
-  task_definition                    = aws_ecs_task_definition.TD1.arn
+  cluster                            = aws_ecs_cluster.ECS2.id
+  task_definition                    = aws_ecs_task_definition.TD2.arn
   scheduling_strategy                = "REPLICA"
   desired_count                      = 2
   deployment_minimum_healthy_percent = 100
@@ -12,15 +12,15 @@ resource "aws_ecs_service" "ECS-Service" {
 
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.TG1.arn
-    container_name   = "main-container1"
+    target_group_arn = aws_lb_target_group.TG2.arn
+    container_name   = "main-container2"
     container_port   = 80
   }
 
 
   network_configuration {
     assign_public_ip = true
-    security_groups  = [aws_security_group.SG1.id]
-    subnets          = [aws_subnet.subnet11.id, aws_subnet.subnet12.id]
+    security_groups  = [aws_security_group.SG2.id]
+    subnets          = [aws_subnet.subnet111.id, aws_subnet.subnet112.id]
   }
 }

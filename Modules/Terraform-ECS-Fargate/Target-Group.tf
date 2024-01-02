a3d9ee868568e87355721f48b39743fb4f87099b
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2024-01-02 07:05:53 +0530

    Update Target-Group.tf

--- Modules/Terraform-ECS-Fargate/Target-Group.tf
+++ Modules/Terraform-ECS-Fargate/Target-Group.tf
@@ -3,7 +3,7 @@ resource "aws_lb_target_group" "TG1" {
   port        = "80"
   protocol    = "HTTP"
   target_type = "ip"
-  vpc_id      = aws_vpc.vpc1.id
+  vpc_id      = aws_vpc.Main-VPC1.id
 
   tags = {
     Name = "Main-TG1"

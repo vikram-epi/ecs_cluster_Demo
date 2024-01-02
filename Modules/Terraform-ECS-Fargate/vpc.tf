bb83ce31c5ea4de108a83d7596f91428d36e1208
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2024-01-02 08:05:56 +0530

    Update vpc.tf

--- Modules/Terraform-ECS-Fargate/vpc.tf
+++ Modules/Terraform-ECS-Fargate/vpc.tf
@@ -1,60 +1,59 @@
-resource "aws_vpc" "Main-VPC1" {
+resource "aws_vpc" "Main-VPC2" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "Main-VPC1"
+    Name = "Main-VPC2"
   }
 }
 
-resource "aws_subnet" "subnet11" {
-  vpc_id                  = aws_vpc.Main-VPC1.id
+resource "aws_subnet" "subnet111" {
+  vpc_id                  = aws_vpc.Main-VPC2.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
 
   tags = {
-    Name = "Main-Subnet11"
+    Name = "Main-Subnet111"
   }
 }
 
 
-resource "aws_subnet" "subnet12" {
-  vpc_id                  = aws_vpc.Main-VPC1.id
+resource "aws_subnet" "subnet112" {
+  vpc_id                  = aws_vpc.Main-VPC2.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1b"
 
   tags = {
-    Name = "Main-Subnet12"
+    Name = "Main-Subnet112"
   }
 }
 
-resource "aws_internet_gateway" "IG1" {
-  vpc_id = aws_vpc.Main-VPC1.id
+resource "aws_internet_gateway" "IG2" {
+  vpc_id = aws_vpc.Main-VPC2.id
 
   tags = {
-    Name = "Main-Internet-Gateway12"
+    Name = "Main-Internet-Gateway112"
   }
 }
 
 
-resource "aws_route_table" "RT1" {
-  vpc_id = aws_vpc.Main-VPC1.id
+resource "aws_route_table" "RT111" {
+  vpc_id = aws_vpc.Main-VPC2.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.IG1.id
+    gateway_id = aws_internet_gateway.IG2.id
   }
 }
 
 
-resource "aws_route_table_association" "RTA11" {
-  subnet_id      = aws_subnet.Main-Subnet11.id
-  route_table_id = aws_route_table.RT1.id
+resource "aws_route_table_association" "RTA111" {
+  subnet_id      = aws_subnet.Main-Subnet111.id
+  route_table_id = aws_route_table.RT111.id
 }
 
 
-resource "aws_route_table_association" "RTA12" {
-  subnet_id      = aws_subnet.Main-Subnet12.id
-  route_table_id = aws_route_table.RT1.id
+resource "aws_route_table_association" "RTA112" {
+  subnet_id      = aws_subnet.Main-Subnet112.id
+  route_table_id = aws_route_table.RT112.id
 }
-

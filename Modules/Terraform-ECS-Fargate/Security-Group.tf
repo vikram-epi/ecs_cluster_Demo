6e392714d07b07fba93b11dad800b8db174a7e3c
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2024-01-02 10:35:51 +0530

    Update Security-Group.tf

--- Modules/Terraform-ECS-Fargate/Security-Group.tf
+++ Modules/Terraform-ECS-Fargate/Security-Group.tf
@@ -21,3 +21,26 @@ resource "aws_security_group" "SG2" {
     Name = "Main-SG2"
   }
 }
+resource "aws_security_group" "SG3" {
+  name        = "Main-SG3"
+  description = "Allow Port 80"
+  vpc_id      = aws_vpc.Main-VPC2.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Main-SG3"
+  }
+}
